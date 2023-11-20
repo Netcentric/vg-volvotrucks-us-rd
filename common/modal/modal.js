a4ae8cc49d17ff2198d38fbe9616b09e1bd64ea8
Tomasz Dziezyk <tomasz.dziezyk@ds.pl>
2023-11-20 12:50:42 +0100

    #76 Add modal heading detection

--- common/modal/modal.js
+++ common/modal/modal.js
@@ -14,6 +14,7 @@ const createModalTopBar = (parentEl) => {
   const topBar = document.createRange().createContextualFragment(`
     <div class="modal-top-bar">
       <div class="modal-top-bar-content">
+        <h2 class="modal-top-bar-heading"></h2>
         <button class="modal-close-button" aria-label=${getTextLabel('close')}>
           <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M4.97979 4.97979C5.17505 4.78453 5.49163 4.78453 5.6869 4.97979L16 15.2929L26.3131 4.97979C26.5084 4.78453 26.825 4.78453 27.0202 4.97979C27.2155 5.17505 27.2155 5.49163 27.0202 5.6869L16.7071 16L27.0202 26.3131C27.2155 26.5084 27.2155 26.825 27.0202 27.0202C26.825 27.2155 26.5084 27.2155 26.3131 27.0202L16 16.7071L5.6869 27.0202C5.49163 27.2155 5.17505 27.2155 4.97979 27.0202C4.78453 26.825 4.78453 26.5084 4.97979 26.3131L15.2929 16L4.97979 5.6869C4.78453 5.49163 4.78453 5.17505 4.97979 4.97979Z" fill="var(--color-icon, #000)"/>
@@ -59,6 +60,28 @@ const createModal = () => {
   const clearModalContent = () => {
     modalContent.innerHTML = '';
     modalContent.className = 'modal-content';
+    modalBackground.querySelector('.modal-top-bar-heading').textContent = '';
+  };
+
+  const handleNewContent = (newContent) => {
+    clearModalContent();
+
+    const firstSection = newContent[0];
+
+    // checking if the first section contains one heading only
+    if (
+      firstSection.children.length === 1
+      && firstSection.children[0].children.length === 1
+      && /^H[1-6]$/.test(newContent[0].children[0].children[0].tagName)
+    ) {
+      const headingContent = firstSection.children[0].children[0].textContent;
+
+      modalBackground.querySelector('.modal-top-bar-heading').textContent = headingContent;
+      firstSection.style.display = 'none';
+    }
+
+    modalContent.classList.add('modal-content--wide');
+    modalContent.append(...newContent);
   };
 
   async function showModal(newContent, { beforeBanner, beforeIframe, modalClasses = [] }) {
@@ -69,9 +92,7 @@ const createModal = () => {
     window.addEventListener('keydown', keyDownAction);
 
     if (newContent && (typeof newContent !== 'string')) {
-      clearModalContent();
-      modalContent.classList.add('modal-content--wide');
-      modalContent.append(...newContent);
+      handleNewContent(newContent);
     } else if (newContent) {
       let videoOrIframe = null;
       if (isLowResolutionVideoUrl(newContent)) {
