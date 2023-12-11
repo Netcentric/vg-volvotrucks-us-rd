b83abdea2a25a5216de380d447f13d375143cc1d
Marko Vukićević <marko.vukicevic@netcentric.biz>
2023-12-11 13:17:30 +0100

    #180 - Add block selectors, unwrap specific divs

--- blocks/v2-media-with-text/v2-media-with-text.js
+++ blocks/v2-media-with-text/v2-media-with-text.js
@@ -1,4 +1,4 @@
-import { variantsClassesToBEM, removeEmptyTags } from '../../scripts/common.js';
+import { variantsClassesToBEM, removeEmptyTags, unwrapDivs } from '../../scripts/common.js';
 import {
   isVideoLink, createVideo, setPlaybackControls, wrapImageWithVideoLink,
   selectVideoLink,
@@ -28,6 +28,15 @@ export function getDynamicVideoHeight(video, playbackControls) {
 
 const variantClasses = ['single-image-1', 'single-video-autoplay-1', 'single-video-modal-1', 'single-image-2-left', 'single-image-2-right', 'double-image-1', 'double-image-2'];
 
+const blockClasses = {
+  singleImage1: `.${blockName}--single-image-1`,
+  singleVideoAutoplay: `.${blockName}--single-video-autoplay-1`,
+  singleVideoModal: `.${blockName}--single-video-modal-1`,
+  singleImage2Left: `.${blockName}--single-image-2-left`,
+  singleImage2Right: `.${blockName}--single-image-2-right`,
+  doubleImage1: `.${blockName}--double-image-1`,
+  doubleImage2: `.${blockName}--double-image-2`,
+};
 export default async function decorate(block) {
   variantsClassesToBEM(block.classList, variantClasses, blockName);
 
@@ -37,7 +46,7 @@ export default async function decorate(block) {
   const headings = block.querySelectorAll('h4');
   headings.forEach((heading) => heading.classList.add(`${blockName}__heading`));
 
-  const paragraphs = block.querySelectorAll('p');
+  const paragraphs = block.querySelectorAll('p:not(:has(picture))');
   paragraphs.forEach((paragraph) => paragraph.classList.add(`${blockName}__paragraph`));
 
   const picture = block.querySelector('picture');
@@ -59,15 +68,6 @@ export default async function decorate(block) {
     link.remove();
   }
 
-  if (picture) {
-    const img = picture.querySelector('img');
-    img.classList.add(`${blockName}__image`);
-    if (picture.parentElement.tagName === 'P') {
-      picture.parentElement.remove();
-    }
-    block.prepend(picture);
-  }
-
   const links = block.querySelectorAll('a');
   const videos = [...links].filter((singleLink) => isVideoLink(singleLink));
 
@@ -86,4 +86,10 @@ export default async function decorate(block) {
   }
 
   removeEmptyTags(block);
+  const blocksCollection = document.querySelectorAll(`${blockClasses.singleImage1}, 
+        ${blockClasses.singleVideoAutoplay}, ${blockClasses.singleVideoModal}, ${blockClasses.doubleImage1}, ${blockClasses.doubleImage2}`);
+
+  blocksCollection.forEach((element) => {
+    unwrapDivs(element);
+  });
 }
